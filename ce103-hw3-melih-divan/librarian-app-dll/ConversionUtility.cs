30b1f081a7f634c2668056bea0ff1b8cdbb86344
Melihdvn <melihdvn@gmail.com>
2022-12-07 21:51:50 +0300

    IsNumeric function moved to own lib

--- ce103-hw3-melih-divan/librarian-app-dll/ConversionUtility.cs
+++ ce103-hw3-melih-divan/librarian-app-dll/ConversionUtility.cs
@@ -9,13 +9,6 @@ namespace LibraryManagement
 {
     public static class ConversionUtility
     {
-        public static bool IsNumeric(object Expression)
-        {
-            double retNum;
-
-            bool isNum = Double.TryParse(Convert.ToString(Expression), System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out retNum);
-            return isNum;
-        }
         public static byte[] ToByte(this string hexString)
         {
             return Enumerable
